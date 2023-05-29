03e95212c659ca0bf5182d5c63167c1fc8c6d191
raoulpicconi <raoulpicconi03@gmail.com>
2023-05-29 17:47:51 +0200

    Fix

--- src/altr-fee-manager.ts
+++ src/altr-fee-manager.ts
@@ -1,4 +1,4 @@
-import { FeeReceived, RebateReceived, Received, RedemptionFeePaid } from "../generated/FakeAltroFeeManager/FakeAltroFeeManager";
+import { FeeReceived, RebateReceived, Received, RedemptionFeePaid } from "../generated/AltrFeeManager/AltrFeeManager";
 
 export function handleFeeReceived(event: FeeReceived): void { }
 export function handleRebateReceived(event: RebateReceived): void { }
