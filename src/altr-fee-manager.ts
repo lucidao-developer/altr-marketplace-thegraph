6bacd6cc3f7cb77478f6fb39d77cd9cb7e4ca9f3
raoulpicconi <raoulpicconi03@gmail.com>
2023-05-29 17:44:16 +0200

    Fix

--- src/altr-fee-manager.ts
+++ src/altr-fee-manager.ts
@@ -1,2 +1,6 @@
-import {} from "../generated/AltrFeeManager/AltrFeeManager";
-import {} from "../generated/schema";
+import { FeeReceived, RebateReceived, Received, RedemptionFeePaid } from "../generated/FakeAltroFeeManager/FakeAltroFeeManager";
+
+export function handleFeeReceived(event: FeeReceived): void { }
+export function handleRebateReceived(event: RebateReceived): void { }
+export function handleReceived(event: Received): void { }
+export function handleRedemptionFeePaid(event: RedemptionFeePaid): void { }
---
+++ src/token-splitter.ts
@@ -0,0 +1,3 @@
+import { TokensReleased } from "../generated/templates/TimedTokenSplitter/TimedTokenSplitter";
+
+export function handleTokensReleased(event: TokensReleased): void { }
